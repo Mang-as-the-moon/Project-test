6756dd356ba58dd0d4ccce6a93bfd44321044c8d
Mohammad <mohammadshariati99990@gmail.com>
2024-02-02 01:15:19 +0300

    Initial Commit

---
+++ messenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  messenger
+//
+//  Created by Mohmammad Sharıatı on 2.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ messenger/messengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerApp.swift
+//  messenger
+//
+//  Created by Mohmammad Sharıatı on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct messengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
